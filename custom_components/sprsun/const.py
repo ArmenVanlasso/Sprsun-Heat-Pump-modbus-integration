8202ea7e60e6761b1a4ddd8c960280b78cd78e39
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-03 17:57:37 +0100

    Update const.py

--- custom_components/sprsun/const.py
+++ custom_components/sprsun/const.py
@@ -21,9 +21,40 @@ MODELS = {
     "CGK-060V3L": "cgk_060v3l",
 }
 
-# Twój jeden sensor (wspólny dla wszystkich modeli)
-# index 188, modbus_address 40189 -> offset 188 (0-based)
-REG_RETURN_TEMP = 188
+# Lista sensorów:
+REG_RETURN_TEMP = 188 # modbus 40189
+REG_temperatura_zasilania = 189 # modbus 40190
+REG_temperatura_zewnetrzna = 190 # modbus 40191
+REG_temperatura_sprezania = 191 # modbus 40192
+REG_temperatura_ssania = 192 # modbus 40193
+REG_cisnienie_sprezania = 193 # modbus 40194
+REG_cisnienie_ssania = 194 # modbus 40195
+REG_temperatura_cwu = 195 # modbus 40196
+REG_temperatura_parownika = 196 # modbus 40197
+REG_y1_wentylator_nastawa = 197 # modbus 40198
+REG_y3_pwm_pompa_obiegowa = 198 # modbus 40199
+REG_wentylator_1_sterowanie = 199 # modbus 40200
+REG_wentylator_1_pomiar = 200 # modbus 40201
+REG_wentylator_2_sterowanie = 201 # modbus 40202
+REG_wentylator_2_pomiar = 202 # modbus 40203
+REG_wymagana_wydajnosc = 203 # modbus 40204
+REG_aktualna_wymagana_wydajnosc = 204 # modbus 40205
+REG_obroty_sprezarki = 205 # modbus 40206
+REG_zawor_eev = 207 # modbus 40208
+REG_status = 209 # modbus 40210
+RE_zabezpieczenie = 210 # modbus 40211
+REG_przegrzanie = 211 # modbus 40212
+REG_tryb_pracy_pompy = 215 # modbus 40216
+REG_temperatura_tloczenia = 216 # modbus 40217
+REG_status = 217 # modbus 40218
+REG_wersja_programu_1 = 325 # modbus 40326
+REG_wersja_programu_2 = 326 # modbus 40327
+REG_wersja_programu_3 = 327 # modbus 40328
+REG_pompa_model_1 = 328 # modbus 40329
+REG_pompa_model_2 = 329 # modbus 40330
+REG_moc_pobierana = 333 # modbus 40334
+REG_napiecie_falownika = 334 # modbus 40335
+REG_prad_falownika = 335 # modbus 40336
 
 # Platformy obsługiwane przez integrację
 PLATFORMS: list[str] = ["sensor"]
