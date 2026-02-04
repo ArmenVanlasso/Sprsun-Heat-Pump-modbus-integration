4f0ebf17b6008e0ed05f802d15841f41787eaf93
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-04 13:39:52 +0100

    Update binary_sensor_cgk_030v3l.py

--- custom_components/sprsun/binary_sensor_cgk_030v3l.py
+++ custom_components/sprsun/binary_sensor_cgk_030v3l.py
@@ -3,31 +3,33 @@
 BINARY_SENSORS_CGK_030V3L = {
     "wy_pompa_obiegowa": {
         "register": 151,
-        "name": "Wyjście: pompa obiegowa",
+        "name": "Pompa obiegowa",
         "on_value": 1,
         "off_value": 0,
+        "icon_on": "mdi:pump",
+        "icon_off": "mdi:pump-off",
     },
     "wy_grzalka_obudowy": {
         "register": 152,
-        "name": "Wyjście: grzałka obudowy",
+        "name": "Grzałka obudowy",
         "on_value": 1,
         "off_value": 0,
     },
     "wy_grzalka_sprezarki": {
         "register": 153,
-        "name": "Wyjście: grzałka sprężarki",
+        "name": "Grzałka sprężarki",
         "on_value": 1,
         "off_value": 0,
     },
     "wy_zawor_3d": {
         "register": 154,
-        "name": "Wyjście: zawór 3‑drogowy",
+        "name": "Zawór 3‑drogowy",
         "on_value": 1,
         "off_value": 0,
     },
     "wy_grzalka": {
         "register": 155,
-        "name": "Wyjście: grzałka",
+        "name": "Grzałka",
         "on_value": 1,
         "off_value": 0,
     },
