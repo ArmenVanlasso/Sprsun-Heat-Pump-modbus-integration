4720ab18ce70781507f294e58053cb11884c9295
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-04 10:14:02 +0100

    Update const.py

--- custom_components/sprsun/const.py
+++ custom_components/sprsun/const.py
@@ -244,4 +244,4 @@ REG_we_ogrzewania = 182 # modbus 10183
 REG_wy_pompa_obiegowa_2 = 183 # modbus 10184
 
 # Platformy obsługiwane przez integrację
-PLATFORMS: list[str] = ["sensor"]
+PLATFORMS: list[str] = ["sensor, binary_sensor"]
