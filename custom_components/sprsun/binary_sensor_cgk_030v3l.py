e3b963e9a0b0188a4d73c6b653576c10c2030e63
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-04 13:04:18 +0100

    Create binary_sensor_cgk_030v3l.py

---
+++ custom_components/sprsun/binary_sensor_cgk_030v3l.py
@@ -0,0 +1,34 @@
+# Binary sensors for model CGK-030V3L
+
+BINARY_SENSORS_CGK_030V3L = {
+    "wy_pompa_obiegowa": {
+        "register": 151,
+        "name": "Wyjście: pompa obiegowa",
+        "on_value": 1,
+        "off_value": 0,
+    },
+    "wy_grzalka_obudowy": {
+        "register": 152,
+        "name": "Wyjście: grzałka obudowy",
+        "on_value": 1,
+        "off_value": 0,
+    },
+    "wy_grzalka_sprezarki": {
+        "register": 153,
+        "name": "Wyjście: grzałka sprężarki",
+        "on_value": 1,
+        "off_value": 0,
+    },
+    "wy_zawor_3d": {
+        "register": 154,
+        "name": "Wyjście: zawór 3‑drogowy",
+        "on_value": 1,
+        "off_value": 0,
+    },
+    "wy_grzalka": {
+        "register": 155,
+        "name": "Wyjście: grzałka",
+        "on_value": 1,
+        "off_value": 0,
+    },
+}
