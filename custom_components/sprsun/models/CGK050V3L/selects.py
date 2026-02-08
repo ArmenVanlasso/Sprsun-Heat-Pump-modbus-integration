79115e9e490e0bfe584c941d433a0fe62d5435e0
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-08 19:53:19 +0100

    Create selects.py

---
+++ custom_components/sprsun/models/CGK050V3L/selects.py
@@ -0,0 +1,77 @@
+ENTITIES = [
+    {
+        "name": "Tryb pracy pompy",
+        "register": 0,
+        "options": {
+            0: "Chłodzenie",
+            1: "Ogrzewanie",
+            2: "CWU",
+            3: "Chłodzenie + CWU",
+            4: "Ogrzewanie + CWU",
+        },
+        "icon": "mdi:heat-pump",
+    },
+    {
+        "name": "Tryb pracy wentylatora",
+        "register": 12,
+        "options": {
+            0: "Dzień",
+            1: "Noc",
+            2: "Krzywa grzewcza",
+            3: "Serwisowy",
+        },
+        "icon": "mdi:fan",
+    },
+    {
+        "name": "Tryb pompy obiegowej",
+        "register": 11,
+        "options": {
+            0: "Ciągła",
+            1: "Zewnętrzne sterowanie",
+            2: "Interwał",
+        },
+        "icon": "mdi:pump",
+    },
+    {
+        "name": "Grzałka",
+        "register": 323,
+        "options": {
+            0: "Wyłączona",
+            1: "Tylko CWU",
+            2: "Tylko ogrzewanie",
+            3: "CWU + ogrzewanie",
+        },
+        "icons": {
+            0: "mdi:radiator-off",
+            1: "mdi:water-boiler",
+            2: "mdi:radiator",
+            3: "mdi:heat-wave",
+        },
+    },
+    {
+        "name": "Funkcja czasowa: Dzień tygodnia",
+        "register": 187,
+        "options": {
+            1: "Poniedziałek",
+            2: "Wtorek",
+            3: "Środa",
+            4: "Czwartek",
+            5: "Piątek",
+            6: "Sobota",
+            7: "Niedziela",
+        },
+        "icon": "mdi:timer-edit",
+    },
+    {
+        "name": "Ustawienia sieciowe",
+        "register": 35,
+        "options": {
+            0: "Slave",
+            1: "Master",
+        },
+        "icons": {
+            0: "mdi:upload-network",
+            1: "mdi:download-network",
+        },
+    },
+]
