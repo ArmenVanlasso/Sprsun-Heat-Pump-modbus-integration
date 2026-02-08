02a983b41e665c215ecc5002e84d59c8a34e7178
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-08 15:20:13 +0100

    Update __init__.py

--- custom_components/sprsun/__init__.py
+++ custom_components/sprsun/__init__.py
@@ -53,7 +53,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         "model": model,
     }
 
-    # Ładujemy platformy (np. sensor)
+    # Ładujemy platformy (np. sensor, binary_sensor)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
