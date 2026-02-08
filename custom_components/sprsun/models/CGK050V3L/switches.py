897bb393d0cb699007dd7f3b9802a2551e718c73
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-08 19:13:18 +0100

    Create switches.py

---
+++ custom_components/sprsun/models/CGK050V3L/switches.py
@@ -0,0 +1,58 @@
+ENTITIES = [
+    {
+        "name": "Włączanie pompy",
+        "register": 40,
+        "icon_on": "mdi:heat-pump",
+        "icon_off": "mdi:heat-pump-offline",
+    },
+    {
+        "name": "Auto start",
+        "register": 2,
+    },
+    {
+        "name": "Tryb CWU",
+        "register": 42,
+    },
+    {
+        "name": "Podgrzewacz sprężarki",
+        "register": 1,
+    },
+    {
+        "name": "Funkcja czasowa: Plan tygodniowy",
+        "register": 38,
+        "icon_on": "mdi:timer-check",
+        "icon_off": "mdi:timer-off",
+    },
+    {
+        "name": "Funkcja czasowa: Plan godzinowy",
+        "register": 39,
+        "icon_on": "mdi:timer-check",
+        "icon_off": "mdi:timer-off",
+    },
+    {
+        "name": "Network EN",
+        "register": 34,
+    },
+    {
+        "name": "Grzałka obudowy",
+        "register": 1,
+        "icon_on": "mdi:radiator",
+        "icon_off": "mdi:radiator-disabled",
+    },
+    {
+        "name": "Automatyczne przełączanie pomiędzy ogrzewaniem a chłodzeniem",
+        "register": 47,
+    },
+    {
+        "name": "Ambient temperature switch",
+        "register": 52,
+        "icon_on": "mdi:home-thermometer",
+        "icon_off": "mdi:home-thermometer-outline",
+    },
+    {
+        "name": "Defrost",
+        "register": 36,
+        "icon_on": "mdi:snowflake-melt",
+        "icon_off": "mdi:snowflake-off",
+    },
+]
