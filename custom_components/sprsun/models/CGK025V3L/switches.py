5ae7e5e82372c6cd68509ead77ece609816b21a9
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-18 15:46:55 +0100

    Update switches.py

--- custom_components/sprsun/models/CGK025V3L/switches.py
+++ custom_components/sprsun/models/CGK025V3L/switches.py
@@ -1,21 +1,27 @@
 ENTITIES = [
     {
-        "name": "Włączanie pompy",
-        "register": 40,
-        "icon_on": "mdi:heat-pump",
-        "icon_off": "mdi:heat-pump-offline",
+        "name": "Podgrzewacz sprężarki",
+        "register": 1,
+        "icon_on": "mdi:radiator",
+        "icon_off": "mdi:radiator-disabled",
     },
     {
         "name": "Auto start",
         "register": 2,
     },
-    {
-        "name": "Tryb CWU",
-        "register": 42,
+    { "name": "Manualne wprowadzenie parametrów BLCD",
+      "register": 14,
+      "icon": "mdi:tune",
     },
     {
-        "name": "Podgrzewacz sprężarki",
-        "register": 1,
+        "name": "Network EN",
+        "register": 34,
+    },
+    {
+        "name": "Defrost",
+        "register": 36,
+        "icon_on": "mdi:snowflake-melt",
+        "icon_off": "mdi:snowflake-off",
     },
     {
         "name": "Funkcja czasowa: Plan tygodniowy",
@@ -30,14 +36,14 @@ ENTITIES = [
         "icon_off": "mdi:timer-off",
     },
     {
-        "name": "Network EN",
-        "register": 34,
+        "name": "Włączanie pompy",
+        "register": 40,
+        "icon_on": "mdi:heat-pump",
+        "icon_off": "mdi:heat-pump-offline",
     },
     {
-        "name": "Grzałka obudowy",
-        "register": 1,
-        "icon_on": "mdi:radiator",
-        "icon_off": "mdi:radiator-disabled",
+        "name": "Tryb CWU",
+        "register": 42,
     },
     {
         "name": "Automatyczne przełączanie pomiędzy ogrzewaniem a chłodzeniem",
@@ -49,10 +55,5 @@ ENTITIES = [
         "icon_on": "mdi:home-thermometer",
         "icon_off": "mdi:home-thermometer-outline",
     },
-    {
-        "name": "Defrost",
-        "register": 36,
-        "icon_on": "mdi:snowflake-melt",
-        "icon_off": "mdi:snowflake-off",
-    },
+
 ]
