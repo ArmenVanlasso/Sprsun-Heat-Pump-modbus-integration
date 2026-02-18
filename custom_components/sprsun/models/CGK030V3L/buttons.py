a73a33666a548e254065e7a78dd63bcaec007b27
Arkadiusz Piasecki <81431935+ArmenVanlasso@users.noreply.github.com>
2026-02-18 15:20:59 +0100

    Create buttons.py

---
+++ custom_components/sprsun/models/CGK030V3L/buttons.py
@@ -0,0 +1,12 @@
+ENTITIES = [
+    {
+        "name": "Przywracanie ustawień fabrycznych pompy",
+        "register": 57,
+        "icon": "mdi:restore",
+    },
+    {
+        "name": "Przywracanie ustawień fabrycznych parametrów sprężarki",
+        "register": 332,
+        "icon": "mdi:restore",
+    },
+]
